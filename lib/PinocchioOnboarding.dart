ae4b6a65ef03f9ea3a5469258758613ef7dbfb9f
LDongGyu <ehdrbchd@naver.com>
2021-01-20 22:14:18 +0900

    feat: onboarding 표지 배치

--- lib/PinocchioOnboarding.dart
+++ lib/PinocchioOnboarding.dart
@@ -19,7 +19,7 @@ class _OnboardingPageState extends State<OnboardingPage> {
                 children: <Widget>[
                   Container(
                     alignment: Alignment.topCenter,
-                    child: Image.asset('images/1.png'),
+                    child: Image.asset('images/3.png'),
                   ),
                   Container(
                     alignment: Alignment.topCenter,
@@ -27,21 +27,26 @@ class _OnboardingPageState extends State<OnboardingPage> {
                   ),
                   Container(
                     alignment: Alignment.topCenter,
-                    child: Image.asset('images/3.png'),
+                    child: Image.asset('images/1.png'),
                   )
                 ],
               ),
             ),
-            Container(
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 250,
                 color: Colors.white,
-                child: Column(children: <Widget>[
-                  Text('옷 세탁법이 궁금하세요?'),
-                  Text('카메라 버튼을 눌러 라벨을 찍어보세요'),
-                  RaisedButton(
-                    child: Text('다음'),
-                    onPressed: () {},
-                  )
-                ]),
+                  child: Column(children: <Widget>[
+                    Text('옷 세탁법이 궁금하세요?'),
+                    Text('카메라 버튼을 눌러 라벨을 찍어보세요'),
+                    RaisedButton(
+                      child: Text('다음'),
+                      onPressed: () {},
+                    )
+                  ]),
+              ),
             )
           ],
         ),
