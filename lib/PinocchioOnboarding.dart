6731083d91f77ca3dc7c1eddecf48f92d0f55f61
LDongGyu <ehdrbchd@naver.com>
2021-01-20 23:14:17 +0900

    feat: pageview indicator 추가

--- lib/PinocchioOnboarding.dart
+++ lib/PinocchioOnboarding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:page_view_indicators/circle_page_indicator.dart';
 
 class OnboardingPage extends StatefulWidget {
   @override
@@ -7,6 +8,8 @@ class OnboardingPage extends StatefulWidget {
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
+  int currentPageValue = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,21 +18,14 @@ class _OnboardingPageState extends State<OnboardingPage> {
           children: <Widget>[
             Container(
               width: MediaQuery.of(context).size.width,
-              child: PageView(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.topCenter,
-                    child: Image.asset('images/3.png'),
-                  ),
-                  Container(
-                    alignment: Alignment.topCenter,
-                    child: Image.asset('images/2.png'),
-                  ),
-                  Container(
-                    alignment: Alignment.topCenter,
-                    child: Image.asset('images/1.png'),
-                  )
-                ],
+              child: PageView.builder(
+                itemCount: onbordingList.length,
+                onPageChanged: (page){
+                  getChangedPageAndMoveBar(page);
+                },
+                itemBuilder: (context, index){
+                  return onbordingList[index];
+                }
               ),
             ),
             Align(
@@ -59,6 +55,23 @@ class _OnboardingPageState extends State<OnboardingPage> {
                   )
                 ),
                 child: Column(children: <Widget>[
+                  Stack(
+                    alignment: AlignmentDirectional.topStart,
+                    children: <Widget>[
+                      Container(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            for (int i = 0; i < onbordingList.length; i++)
+                              if (i == currentPageValue) ...[circleBar(true)] else
+                                circleBar(false),
+                          ],
+                        ),
+                        height: 52,
+                      ),
+                    ],
+                  ),
                   Text('옷 세탁법이 궁금하세요?'),
                   Text('카메라 버튼을 눌러 라벨을 찍어보세요'),
                   RaisedButton(
@@ -73,22 +86,40 @@ class _OnboardingPageState extends State<OnboardingPage> {
       ),
     );
   }
+
+  final List<Widget> onbordingList = <Widget>[
+    Container(
+      alignment: Alignment.topCenter,
+      child: Image.asset('images/3.png'),
+    ),
+    Container(
+      alignment: Alignment.topCenter,
+      child: Image.asset('images/2.png'),
+    ),
+    Container(
+      alignment: Alignment.topCenter,
+      child: Image.asset('images/1.png'),
+    )
+  ];
+
+  Widget circleBar(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      height: 8,
+      width: 8,
+      decoration: BoxDecoration(
+          color: isActive ? Color(0xFFF8D247) : Colors.white,
+          border: Border.all(
+            color : isActive ? Color(0xFFDFB10F) : Color(0xFFC4C4C4),
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(12))),
+    );
+  }
+
+  void getChangedPageAndMoveBar(int page) {
+    currentPageValue = page;
+    setState(() {});
+  }
 }
 
-//
-// PageView(
-// children: <Widget>[
-// Container(
-// alignment: Alignment.topCenter,
-// child: Image.asset('images/1.png'),
-// ),
-// Container(
-// alignment: Alignment.topCenter,
-// child: Image.asset('images/2.png'),
-// ),
-// Container(
-// alignment: Alignment.topCenter,
-// child: Image.asset('images/3.png'),
-// )
-// ],
-// ),
